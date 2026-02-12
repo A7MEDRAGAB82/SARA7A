35248dcb90e8a98fb3d4c48e022ec845769e73c9
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-02-12 23:09:54 +0200

    initialize database service

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,24 +1,26 @@
-import { ProviderEnums } from "../../common/index.js"
-import { ConflictException, NotFoundException } from "../../common/utils/response/error.responce.js"
-import {userModel} from "../../database/index.js"
+import { ProviderEnums } from "../../common/index.js";
+import {
+  ConflictException,
+  NotFoundException,
+} from "../../common/utils/response/error.responce.js";
+import { userModel } from "../../database/index.js";
+import { findOne } from "../../database/database.service.js";
 
+export const signUp = async (data) => {
+  let { userName, email, password } = data;
+  let existUser = await findOne({model:userModel ,filter:{email}})
+  if (existUser) {
+    return ConflictException("email already exist");
+  }
+  let addedUser = await userModel.insertOne({ userName, email, password });
+  return addedUser;
+};
 
-export const signUp = async (data) =>{
-    let {userName , email  , password} = data
-    let existUser = await userModel.findOne({email})
-    if(existUser) {
-        return ConflictException("email already exist")
-    }
-    let addedUser = await userModel.insertOne({userName , email , password})
-    return addedUser
-}
-
-export const login = async (data) =>{
-    let { email  , password} = data
-    let existUser = await userModel.findOne({email , password , provider:ProviderEnums.System})
-    if(existUser) {
-        return existUser
-    }
-    return NotFoundException({message:"invalid email or password"})
-    
-} 
+export const login = async (data) => {
+  let { email, password } = data;
+  let existUser = await findOne({model:userModel , filter:{email , password , provider:ProviderEnums.System} , select:"-password -_id"})
+  if (existUser) {
+    return existUser;
+  }
+  return NotFoundException({ message: "invalid email or password" });
+};
