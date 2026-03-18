deead45748ac3e41a1d94c08b9197710388da764
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-03-18 06:00:31 +0200

    refactor(messages): allow anonymous senders and enhance schema validation constraints

--- src/database/models/message.model.js
+++ src/database/models/message.model.js
@@ -6,16 +6,18 @@ const messageSchema = new Schema(
       type: String,
       required: [true, "Message content is required"],
       trim: true,
+      minLength: [1,"Message cannot be empty"],
+      maxLength: [5000,"Message is too long"]
     },
     senderId: {
       type: Schema.Types.ObjectId,
-      required: true,
+      required: false,
       ref: "User",
       index:true
     },
     receiverId: {
       type: Schema.Types.ObjectId,
-      required: true,
+      required: [true,"receiverId is required"],
       ref: "User",
       index:true
     },
